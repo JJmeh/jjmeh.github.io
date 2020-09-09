921b11cb2672e5af516710a06b376cc255284e62
JJmeh <jeremiah.kaliki@gmail.com>
2020-09-09 16:29:15 +0700

    initial commit

---
+++ precache-manifest.9303402b608bff103bd6b74a4ad39c44.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "170544ff73ccbf6a3e14b795a77d7ca0",
+    "url": "/portfolio-web/index.html"
+  },
+  {
+    "revision": "e2f2cbf4259aafbb50f2",
+    "url": "/portfolio-web/static/css/main.4e5b23b6.chunk.css"
+  },
+  {
+    "revision": "03e5dd7bb83e2ec7bbee",
+    "url": "/portfolio-web/static/js/2.e9dabfb2.chunk.js"
+  },
+  {
+    "revision": "cc44a2e553976c551aab1c9ceb701714",
+    "url": "/portfolio-web/static/js/2.e9dabfb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f2cbf4259aafbb50f2",
+    "url": "/portfolio-web/static/js/main.40369363.chunk.js"
+  },
+  {
+    "revision": "2e11a03bd117c9248eed",
+    "url": "/portfolio-web/static/js/runtime-main.fa8d4299.js"
+  },
+  {
+    "revision": "8a3ffb631d1295c828d29f101c9998ce",
+    "url": "/portfolio-web/static/media/pexels-bri-schneiter-346529.8a3ffb63.jpg"
+  },
+  {
+    "revision": "a605f66e8ce1bc11528440bee48cdc53",
+    "url": "/portfolio-web/static/media/pexels-efdal-yildiz-917494.a605f66e.jpg"
+  },
+  {
+    "revision": "7d7cc8d46bbac8fc6dbf7990224ac322",
+    "url": "/portfolio-web/static/media/pexels-heorhii-heorhiichuk-994883.7d7cc8d4.jpg"
+  },
+  {
+    "revision": "afa928ad078b09223338843502e16ccd",
+    "url": "/portfolio-web/static/media/pexels-jessica-lewis-583846.afa928ad.jpg"
+  },
+  {
+    "revision": "42f7a831df9138ad993edd853e5ca262",
+    "url": "/portfolio-web/static/media/pexels-jessica-lewis-593322.42f7a831.jpg"
+  },
+  {
+    "revision": "55aa4d9e218ac9fff6ca82939c0c1846",
+    "url": "/portfolio-web/static/media/pexels-negative-space-160107.55aa4d9e.jpg"
+  },
+  {
+    "revision": "154aa97353043d2ea9029d88f217b392",
+    "url": "/portfolio-web/static/media/pexels-stephan-seeber-1054218.154aa973.jpg"
+  }
+]);
